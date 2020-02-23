238c62c149f34c085d616de8f68b553176343c4f
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-23 14:39:16 -0700

    add AppProvider

--- src/App/index.js
+++ src/App/index.js
@@ -4,13 +4,16 @@ import WelcomeMessage from './WelcomeMessage';
 import styled, {css} from 'styled-components';
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
+import { AppProvider } from './AppProvider';
 
 class App extends React.Component {
   render() {
     return (
       <AppLayout>
-        <AppBar />
-        <WelcomeMessage />
+        <AppProvider>
+          <AppBar />
+          <WelcomeMessage />
+        </AppProvider>
       </AppLayout>
     );
   }
