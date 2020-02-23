e7bfe3d9cce7cbda5bb7add60ea802b6d61cfc8f
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-23 11:50:04 -0700

    Updates

---
+++ precache-manifest.3c833e8b6a665cc93346f44e6630cb49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6278732b1fe2ab4b3f570d86bacd09",
+    "url": "/cryptodash/index.html"
+  },
+  {
+    "revision": "b904b8310789bdd0a1c7",
+    "url": "/cryptodash/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b925931873d75bc1d47f",
+    "url": "/cryptodash/static/js/2.d0b02b8b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cryptodash/static/js/2.d0b02b8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b904b8310789bdd0a1c7",
+    "url": "/cryptodash/static/js/main.9a918b99.chunk.js"
+  },
+  {
+    "revision": "87ea402619949cdd0724",
+    "url": "/cryptodash/static/js/runtime-main.b591310e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cryptodash/static/media/logo.5d5d9eef.svg"
+  }
+]);
