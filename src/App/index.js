bc2db398b42c459acabd1bd5ffb5ca5116360cac
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-23 13:04:07 -0700

    add AppLayout

--- src/App/index.js
+++ src/App/index.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
 import styled, {css} from 'styled-components';
+import AppLayout from './AppLayout';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <AppLayout>
         <WelcomeMessage />
-      </div>
+      </AppLayout>
     );
   }
 }
