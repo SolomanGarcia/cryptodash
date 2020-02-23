07572dc4de92aa392f5f611f922549657b3e2d0b
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-23 12:58:53 -0700

    add Google Fonts

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Welcome from './WelcomeMessage';
+import WelcomeMessage from './WelcomeMessage';
+import styled, {css} from 'styled-components';
 
 class App extends React.Component {
   render() {
     return (
-      <Welcome />
+      <div>
+        <WelcomeMessage />
+      </div>
     );
   }
 }
