7542994dd5901c40022e153a77d69ab43629e75c
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-24 12:52:37 -0700

    add Search

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -3,13 +3,15 @@ import WelcomeMessage from './WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page';
 import CoinGrid from './CoinGrid';
+import Search from './Search';
 
 export default function() {
   return (
         <Page name="settings">
           <WelcomeMessage /> 
           <CoinGrid topSection />
-          <ConfirmButton /> 
+          <ConfirmButton />
+          <Search />
           <CoinGrid />
         </Page>
   ); 
