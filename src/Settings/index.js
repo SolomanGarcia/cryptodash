dfa2e918b0ba9fd88e0b013d16fd65fb268cbb6e
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-23 18:01:36 -0700

    add CoinGrid

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page';
+import CoinGrid from './CoinGrid';
 
 export default function() {
   return (
         <Page name="settings">
         <WelcomeMessage /> 
         <ConfirmButton /> 
+        <CoinGrid />
         </Page>
   ); 
 };
