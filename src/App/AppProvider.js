2989dcd84e3ae9939d66ece48e9de1c28720ea2f
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-24 12:24:20 -0700

    Finish add/remove coins

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,28 +1,50 @@
 import React from 'react';
+import _ from 'lodash';
 
 const cc = require('cryptocompare');
 
 export const AppContext = React.createContext();
+
+const MAX_FAVORITES = 10;
 
 export class AppProvider extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             page: 'dashboard',
+            favorites: ['BTC', 'ETH', 'XMR', 'DOGE'],
             ...this.savedSettings(),
             setPage: this.setPage,
+            addCoin: this.addCoin,
+            removeCoin: this.removeCoin,
+            isInFavorites: this.isInFavorites,
             confirmFavorites: this.confirmFavorites
         }
     }
 
     componentDidMount = () => {
         this.fetchCoins();
-    };
+    }
 
     fetchCoins = async () => {
         let coinList = (await cc.coinList()).Data;
         this.setState({coinList});
-    };
+    }
+
+    addCoin = key => {
+        let favorites = [...this.state.favorites];
+        if(favorites.length < MAX_FAVORITES) {
+            favorites.push(key);
+            this.setState({favorites});
+        }
+    }
+
+    removeCoin = key => {
+        let favorites = [...this.state.favorites];
+        this.setState({favorites: _.pull(favorites, key)})
+    }
+
+    isInFavorites = key => _.includes(this.state.favorites, key)
 
     confirmFavorites = () => {
         this.setState({
@@ -30,7 +52,7 @@ export class AppProvider extends React.Component {
             page: 'dashboard'
         });
         localStorage.setItem('cryptoDash', JSON.stringify({
-            test: 'Hello'
+            favorites: this.state.favorites
         }));
     };
 
@@ -39,7 +61,8 @@ export class AppProvider extends React.Component {
         if (!cryptoDashData) {
             return { page: 'settings', firstVisit: true };
         }
-        return {};
+        let {favorites} = cryptoDashData;
+        return {favorites};
     }
 
     setPage = page => this.setState({page});
--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -4,19 +4,34 @@ import { SelectableTile, DisabledTile, DeletableTile } from '../Shared/Tile';
 import CoinHeaderGrid from './CoinHeaderGrid';
 import CoinImage from '../Shared/CoinImage';
 
+function clickCoinHandler(topSection, coinKey, addCoin, removeCoin) {
+    return topSection ? () => {
+        removeCoin(coinKey)
+    } : () => {
+        addCoin(coinKey)
+    }
+}
+
 export default function({coinKey, topSection}) {
 
     return <AppContext.Consumer>
-        {({coinList}) =>{
+        {({coinList, addCoin, removeCoin, isInFavorites}) =>{
             let coin = coinList[coinKey];
 
             let TileClass = SelectableTile;
             if(topSection) {
                 TileClass = DeletableTile;
+            } else if(isInFavorites(coinKey)) {
+                TileClass = DisabledTile;
             }
 
-            return <TileClass>
-                <CoinHeaderGrid topSection={topSection} name={coin.CoinName} symbol={coin.symbol} />
+            return <TileClass
+                onClick={clickCoinHandler(topSection, coinKey, addCoin, removeCoin)}
+            >
+                <CoinHeaderGrid
+                    topSection={topSection} 
+                    name={coin.CoinName} 
+                    symbol={coin.symbol} />
                 <CoinImage coin={coin} />
             </TileClass>
         }}
