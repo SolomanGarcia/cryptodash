1e54ebcc62859d988d05a14ee9c82eddd7b5e66f
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-23 17:39:23 -0700

    add Page.js

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
+import Page from '../Shared/Page';
 
 export default function() {
-  return <div> <WelcomeMessage /> <ConfirmButton /> </div> 
+  return (
+        <Page name="settings">
+        <WelcomeMessage /> 
+        <ConfirmButton /> 
+        </Page>
+  ); 
 };
