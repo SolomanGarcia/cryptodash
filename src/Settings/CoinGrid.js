82ba244662b0443dafd2a1fed20fe7c967f385c4
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-24 08:45:19 -0700

    create CoinGrid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
 import { AppContext } from '../App/AppProvider';
+import { SelectableTile } from "../Shared/Tile";
 
 export const CoinGridStyled = styled.div`
     display: grid;
     grid-template-columns: repeat(5, 1fr);
+    grid-gap: 15px;
 `;
+
+function getCoinsToDisplay(coinList) {
+    return Object.keys(coinList).slice(0, 100);
+}
 
 export default function() {
     return (
         <AppContext.Consumer>
             {({coinList}) => <CoinGridStyled>
-                {Object.keys(coinList).map(coinKey => <div> {coinKey} </div>)}
-                </CoinGridStyled>}
+                {getCoinsToDisplay(coinList).map(coinKey => 
+                    <SelectableTile> {coinKey} </SelectableTile>
+                )}
+            </CoinGridStyled>}
         </AppContext.Consumer>
     );
 }
