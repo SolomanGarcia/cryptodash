6e0461e19088bdda35f850d372780efc3da96584
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-24 13:50:24 -0700

    add fuzzy search

--- src/Settings/Search.js
+++ src/Settings/Search.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { backgroundColor2, fontSize2 } from '../Shared/Styles';
+import { AppContext } from '../App/AppProvider';
+import _ from 'lodash';
+import fuzzy from 'fuzzy';
 
 const SearchGrid = styled.div`
     display: grid;
@@ -16,11 +19,39 @@ const SearchInput = styled.input`
     place-self: center left;
 `;
 
+const handleFilter = _.debounce(({inputValue, coinList, setfilterCoins}) => {
+    // Get all coin symbols
+    let coinSymbols = Object.keys(coinList);
+    // Get all the coin names and map symbol to name
+    let coinNames = coinSymbols.map(sym => coinList[sym].coinName)
+    let allStringsToSearch = coinSymbols.concat(coinNames);
+    let fuzzyResults = fuzzy.filter(inputValue, allStringsToSearch, {}).map(result => result.string);
+    let filteredCoins = _.pickBy(coinList, (result, symKey) => {
+        let coinName = result.coinName;
+        return (_.includes(fuzzyResults, symKey) || _.includes(fuzzyResults, coinName));
+    });
+
+    setfilterCoins(filteredCoins);
+}, 500);
+
+function filterCoins(e, setFilteredCoins, coinList) {
+    let inputValue = e.target.value;
+    if(!inputValue) {
+        setFilteredCoins(null);
+        return;
+    }
+    handleFilter(inputValue, coinList, setFilteredCoins);
+}
+
 export default function() {
     return (
-    <SearchGrid>
-        <h2>Search all coins</h2>
-        <SearchInput />
-    </SearchGrid>
+    <AppContext.Consumer>
+        {({setFilteredCoins, coinList}) =>
+            <SearchGrid>
+                <h2>Search all coins</h2>
+                <SearchInput onKeyUp={(e) => filterCoins(e, setFilteredCoins, coinList)} />
+            </SearchGrid>
+        }
+    </AppContext.Consumer>
     );
 }
