1a2dc76978cea0e156856469aaad2b61bb758d9c
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-24 09:33:42 -0700

    debug

--- src/App/index.js
+++ src/App/index.js
@@ -6,7 +6,7 @@ import { AppProvider } from './AppProvider';
 import Settings from '../Settings';
 import Content from '../Shared/Content';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <AppLayout>
