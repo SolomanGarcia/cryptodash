b4a651097a437415ce34204a014bd873c44d5a57
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-24 20:24:29 -0700

    add PriceTile

---
+++ src/Dashboard/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Page from '../Shared/Page';
+import PriceGrid from './PriceGrid';
+
+export default function() {
+  return <Page name="dashboard">
+    <PriceGrid />
+  </Page>  
+};
