330259a27e582b8491654e617c5d38d9dd01b185
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-25 09:24:41 -0700

    update/debug AppBar.js

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,50 +1,54 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
-import { AppContext } from './AppProvider';
+import {AppContext} from "./AppProvider";
 
 const Logo = styled.div`
-    font-size: 1.5em;
-`;
+  font-size: 1.5em; 
+`
 
 const Bar = styled.div`
-    display: grid;
-    margin-bottom: 40px;
-    grid-template-columns: 180px auto 100px 100px;
-`;
+  display: grid; 
+  margin-bottom: 40px; 
+  grid-template-columns: 180px auto 100px 100px; 
+`
 
-const ControlButtonElement = styled.div`
-    cursor: pointer;
-    ${props => props.active && css`
-        text-shadow: 0px 0px 60px #03ff03;
-    `}
-`;
+const ControlButtonElem = styled.div`
+  cursor: pointer; 
+  ${props => props.active && css`
+    text-shadow: 0px 0px 60px #03ff03;
+  `}
+  ${props => props.hidden && css`
+    display: none; 
+  `}
+`
 
-function toProperCase(lower) {
-    return lower.charAt(0).toUpperCase() + lower.substr(1);
-};
+function toProperCase(lower){
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
 
-function ControlButton({name, active}) {
-    return (
-        <AppContext.Consumer>
-            {({page, setPage}) => (
-            <ControlButtonElement
-             active={page === name}
-             onClick={()=> setPage(name)}
-             >
-                {toProperCase(name)}
-            </ControlButtonElement>
-            )}
-        </AppContext.Consumer>
-    );
-};
+function ControlButton({name}){
+  return (
+    <AppContext.Consumer>
+      {({firstVisit, page, setPage}) => (
+        <ControlButtonElem
+          active={page === name}
+          onClick={()=> setPage(name)}
+          hidden={firstVisit && name === 'dashboard'}
+        >
+          {toProperCase(name)}
+        </ControlButtonElem>
+        )}
+    </AppContext.Consumer>
+  )
+}
 
-export default function() {
-    return (
-        <Bar>
-            <Logo>CryptoDash</Logo>
-            <div></div>
-            <ControlButton active name="dashboard" />
-            <ControlButton name="settings" />
-        </Bar>
-    );
-};
+export default function(){
+  return (
+    <Bar>
+      <Logo> CryptoDash </Logo>
+      <div/>
+      <ControlButton active name="dashboard"/>
+      <ControlButton name="settings"/>
+    </Bar>
+  );
+}
