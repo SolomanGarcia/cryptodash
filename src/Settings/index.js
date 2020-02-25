c40b0068985ff1324cdedaecd0308eab63d8354e
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-25 09:34:03 -0700

    update/debug Settings index.js

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import WelcomeMessage from './WelcomeMessage';
+import WelcomeMessage from './WelcomeMessage'
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page';
 import CoinGrid from './CoinGrid';
 import Search from './Search';
 
-export default function() {
+export default function(){
   return <Page name="settings">
-    <WelcomeMessage /> 
-    <CoinGrid topSection />
-    <ConfirmButton />
-    <Search />
-    <CoinGrid />
-  </Page>  
-};
+    <WelcomeMessage/>
+    <CoinGrid topSection/>
+    <ConfirmButton/>
+    <Search/>
+    <CoinGrid/>
+  </Page>
+}
