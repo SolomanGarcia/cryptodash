07b28ab5321ad983b2ec8289e6dcf8396aa941b0
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-25 12:09:25 -0700

    debug

--- src/Dashboard/ChartSelect.js
+++ src/Dashboard/ChartSelect.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 import {backgroundColor2, fontSize2} from "../Shared/Styles";
 
 export default styled.select`
-  // ${backgroundColor2};
-  // ${fontSize2};
+  ${backgroundColor2};
+  ${fontSize2};
   color: #1163c9; 
   border: 1px solid;
   float: right; 
