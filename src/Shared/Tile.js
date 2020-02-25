e8a4ff774bf9e2df1d4384fa8eabbb556ad4713d
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-25 09:48:57 -0700

    update/debug Tile.js

--- src/Shared/Tile.js
+++ src/Shared/Tile.js
@@ -1,30 +1,27 @@
-
 import styled from 'styled-components';
-import { subtleBoxShadow, lightBlueBackground, greenBoxShadow, redBoxShadow } from './Styles';
+import {subtleBoxShadow, lightBlueBackground, greenBoxShadow, redBoxShadow} from "./Styles";
 
 export const Tile = styled.div`
-    background-color: #061a44;
-    box-shadow: 0px 0px 5px 1px #121d5b;
-    // ${subtleBoxShadow}
-    // ${lightBlueBackground}
-    padding: 10px;   
-`;
+  ${subtleBoxShadow}
+  ${lightBlueBackground}
+  padding: 10px;   
+`
 
 export const SelectableTile = styled(Tile)`
-    &:hover {
-        cursor: pointer;
-        ${greenBoxShadow}
-    }
-`;
+  &:hover {
+    cursor: pointer; 
+    ${greenBoxShadow}
+  }
+`
 
 export const DeletableTile = styled(SelectableTile)`
-    &:hover {
-        cursor: pointer;
-        ${redBoxShadow}
-    }
+	&:hover{
+		cursor: pointer; 
+		${redBoxShadow}
+	}
 `;
 
 export const DisabledTile = styled(Tile)`
-    pointer-events: none;
-    opacity: 0.4;
+	pointer-events: none;
+  opacity: 0.4;
 `;
