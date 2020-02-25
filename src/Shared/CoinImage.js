75290639274732770785ce81f194a46bdb6a08d4
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-25 09:44:31 -0700

    update/debug CoinImage.js

--- src/Shared/CoinImage.js
+++ src/Shared/CoinImage.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled, {css} from 'styled-components';
 
-export default function({coin, style}) {
-    return <img
-        alt={coin.CoinSymbol}
-        style={style || {height: '50px'}}
-        src={`http://cryptocompare.com/${
-            coin.ImageUrl
-        }`}
-    />;
+const CoinImage = styled.img`
+  height: 50px; 
+  ${props => props.spotlight && css`
+    height: 200px; 
+    margin: auto; 
+    display: block; 
+  `}
+`
+
+
+export default function ({coin, spotlight}) {
+  return <CoinImage
+    spotlight={spotlight}
+    alt={coin.CoinSymbol}
+    src={`http://cryptocompare.com/${
+      coin.ImageUrl
+      }`}
+  />;
 }
