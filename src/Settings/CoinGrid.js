281046bc5125cbe7dd77a108d32a9acf7aa05d7c
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-25 09:29:07 -0700

    update/debug CoinGrid.js

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
-import { AppContext } from '../App/AppProvider';
-import CoinTile from './CoinTile';
+import {AppContext} from "../App/AppProvider";
+import CoinTile from "./CoinTile";
 
 export const CoinGridStyled = styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
-    grid-gap: 15px;
-    margin-top: 40px;
-`;
+  display: grid;   
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr)); 
+  grid-gap: 15px; 
+  margin-top: 40px; 
+`
 
-function getLowerSectionCoins(coinList, filteredCoins) {
-    return (filteredCoins && Object.keys(filteredCoins)) || Object.keys(coinList).slice(0, 100)
+function getLowerSectionCoins(coinList, filteredCoins){
+  return (filteredCoins && Object.keys(filteredCoins)) ||
+    Object.keys(coinList).slice(0, 100)
 }
 
-function getCoinsToDisplay(coinList, topSection, favorites, filterCoins) {
-    return topSection ? favorites : getLowerSectionCoins(coinList, filterCoins);
+function getCoinsToDisplay(coinList, topSection, favorites, filterCoins){
+  return topSection ? favorites : getLowerSectionCoins(coinList, filterCoins);
 }
 
-export default function({topSection}) {
-    return <AppContext.Consumer>
-        {({coinList, favorites, filteredCoins}) => <CoinGridStyled>
-            {getCoinsToDisplay(coinList, topSection, favorites, filteredCoins).map(coinKey => 
-                <CoinTile topSection={topSection} coinKey={coinKey} />
-            )}
-        </CoinGridStyled>}
-    </AppContext.Consumer>
+export default function ({topSection}){
+  return (
+    <AppContext.Consumer>
+    {({coinList, favorites, filteredCoins}) => (
+      <CoinGridStyled>
+        {getCoinsToDisplay(coinList, topSection, favorites, filteredCoins).map(coinKey =>
+          <CoinTile key={coinKey} topSection={topSection} coinKey={coinKey}/>
+        )}
+      </CoinGridStyled>
+    )}
+  </AppContext.Consumer>
+  );
 }
