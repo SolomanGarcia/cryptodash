a97570c0e264d026f0c4d4dc442f0e92b967612c
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-24 19:40:27 -0700

    add fetch prices

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -25,11 +25,33 @@ export class AppProvider extends React.Component {
 
     componentDidMount = () => {
         this.fetchCoins();
+        this.fetchPrices();
     }
 
     fetchCoins = async () => {
         let coinList = (await cc.coinList()).Data;
         this.setState({coinList});
+    }
+
+    fetchPrices = async () => {
+        if(this.state.firstVisit) return;
+        let prices = await this.prices();
+        this.setState({prices});
+        prices = prices.filter(price => Object.keys(price).length);
+        this.setState({prices});
+    }
+
+    prices = async () => {
+        let returnData = [];
+        for(var i = 0; i < this.state.favorites.length; i++) {
+            try {
+                let priceData = await cc.priceFull(this.state.favorites[i], 'USD');
+                returnData.push(priceData);
+            } catch (e) {
+                console.warn('Fetch price error: ', e);
+            }
+        }
+        return returnData;
     }
 
     addCoin = key => {
@@ -51,6 +73,8 @@ export class AppProvider extends React.Component {
         this.setState({
             firstVisit: false,
             page: 'dashboard'
+        }, () => {
+            this.fetchPrices();
         });
         localStorage.setItem('cryptoDash', JSON.stringify({
             favorites: this.state.favorites
