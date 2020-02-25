ea72c8c94341a2d29b07674c01c7d39ee421c8b7
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-25 09:51:11 -0700

    update/debug Dashboard index.js

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 import Page from '../Shared/Page';
 import PriceGrid from './PriceGrid';
+import CoinSpotlight from './CoinSpotlight';
+import PriceChart from './PriceChart';
 
-export default function() {
+const ChartGrid = styled.div`
+  display: grid; 
+  margin-top: 20px; 
+  grid-gap: 15px; 
+  grid-template-columns: 1fr 3fr; 
+`
+
+export default function(){
   return <Page name="dashboard">
-    <PriceGrid />
-  </Page>  
-};
+    <PriceGrid/>
+    <ChartGrid>
+      <CoinSpotlight/>
+      <PriceChart/>
+    </ChartGrid>
+  </Page>
+}
