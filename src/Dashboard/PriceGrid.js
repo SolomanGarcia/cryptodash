0b06076b49dfe2bd2ea34d85a044090a7a6be935
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-25 09:52:49 -0700

    update/debug PriceGrid.js

--- src/Dashboard/PriceGrid.js
+++ src/Dashboard/PriceGrid.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-import { AppContext } from '../App/AppProvider';
+import {AppContext} from "../App/AppProvider";
 import PriceTile from './PriceTile';
 
 const PriceGrid = styled.div`
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-gap: 15px;
-    margin-top: 40px;
-`;
+  display: grid; 
+  grid-template-columns: repeat(5, 1fr); 
+  grid-gap: 15px; 
+  margin-top: 40px; 
+`
 
-export default function() {
-    return (
-        <AppContext.Consumer>
-            {({prices}) => (
-                <PriceGrid>
-                    {prices.map((price, index) => (
-                        <PriceTile index={index} price={price}/>
-                    ))}
-                </PriceGrid>
-            )}
-        </AppContext.Consumer>
-    );
+export default function () {
+  return (
+    <AppContext.Consumer>
+      {({prices}) => (
+        <PriceGrid>
+          {prices.map((price, index) => (
+            <PriceTile key={`priceTile-${index}`} index={index} price={price}/>
+          ))}
+        </PriceGrid>
+      )}
+    </AppContext.Consumer>
+  );
 }
