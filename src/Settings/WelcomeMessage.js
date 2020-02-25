14e38d26c53afd973356e1475f03a62c9a527926
Soloman Garcia <soloman.garcia@gmail.com>
2020-02-25 09:35:59 -0700

    update/debug WelcomeMessage.js

--- src/Settings/WelcomeMessage.js
+++ src/Settings/WelcomeMessage.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { AppContext } from '../App/AppProvider';
+import {AppContext} from "../App/AppProvider";
 
 export default function ({firstVisit}) {
-    return (
-      <AppContext.Consumer>
-        {({firstVisit}) => 
-          firstVisit ? <div>
-            Welcome to CryptoDash, please select your favorite coins to begin.{' '}
-          </div> : null
-        }
-      </AppContext.Consumer>
-    );
-  };
-  
+  return (
+    <AppContext.Consumer>
+      {({firstVisit}) =>
+        firstVisit ? <div>
+          Welcome to CryptoDash, please select your favorite coins to begin.{' '}
+        </div> : null
+      }
+    </AppContext.Consumer>
+  );
+};
